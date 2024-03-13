b452ba68b981357f44ae44d2c0f477f5f561da88
adeqoou <aidarbekovadahan*@gmail.com>
2024-03-13 22:27:58 +0300

    com2

--- gdzapp/apps.py
+++ gdzapp/apps.py
@@ -1,12 +1,18 @@
 from django.apps import AppConfig
+<<<<<<< HEAD
 from .parser import parse_data
+=======
+>>>>>>> e02052c (com2)
 
 
 class GdzappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'gdzapp'
+<<<<<<< HEAD
 
     def ready(self):
         from . import models
         parse_data(models)
 
+=======
+>>>>>>> e02052c (com2)
