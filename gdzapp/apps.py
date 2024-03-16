5cb3d01a6bd84b1acacdf7772d479cfba9dd9512
MeldyTheCoder <cool.groshelev@mail.ru>
2024-03-16 04:32:59 +0300

    v 1.2 - добавлен парсер данных учеников

--- gdzapp/apps.py
+++ gdzapp/apps.py
@@ -1,12 +1,7 @@
 from django.apps import AppConfig
-from .parser import parse_data
 
 
 class GdzappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'gdzapp'
-
-    def ready(self):
-        from . import models
-        parse_data(models)
 
--- gdzapp/urls.py
+++ gdzapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('api/book/', views.BookAPIView.as_view(), name='book_api'),
+    path('api/book/fill/', views.BookAddRequestView.as_view(), name='book_api_fill')
 ]
