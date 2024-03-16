87851d8ae677b68c8211d5a3a1e6cc117eb4290d
adeqoou <aidarbekovadahan*@gmail.com>
2024-03-16 04:57:59 +0300

    changes 2

--- gdzapp/views.py
+++ gdzapp/views.py
@@ -1,18 +1,26 @@
 from .serializers import *
 from rest_framework import generics
 from . import models
-from parser import parse_data
+from .parser import parse_data
 from rest_framework.response import Response
+from django.shortcuts import render
 
 
 class BookAPIView(generics.ListAPIView):
     queryset = models.Book.objects.all()
     serializer_class = BookSerializer
 
+#
+# def request_db(request):
+#     my_data = Cover.objects.all()
+#     return render('gdzapp/api1.html', {})
 
-class BookAddRequestView(generics.CreateAPIView):
-    def post(self, request, *args, **kwargs):
+
+class BookAddRequestView(generics.RetrieveAPIView):
+    serializer_class = BookSerializer
+
+    def get_queryset(self):
         parse_data(models)
 
         books = models.Book.objects.all()
-        return Response(data=BookSerializer(books, many=True).data)
+        return books
